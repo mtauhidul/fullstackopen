d5a30ccc065c7e8f494d32da9a82fda68e182bdb
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-04 05:38:10 +0600

    conditional rendering for unicafe added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,6 @@
 import { useState } from "react";
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  function onGood() {
-    const updatedCount = good + 1;
-    setGood(updatedCount);
-  }
-
-  function onNeutral() {
-    const updatedCount = neutral + 1;
-    setNeutral(updatedCount);
-  }
-
-  function onBad() {
-    const updatedCount = bad + 1;
-    setBad(updatedCount);
-  }
-
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
 
   function averageScore() {
@@ -46,6 +26,45 @@ const App = () => {
 
     return percentage;
   }
+  return (
+    <>
+      <h1>statistics</h1>
+      {all > 0 ? (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {averageScore() || 0}</p>
+          <p>positive {positivePercentage() || 0}%</p>
+        </>
+      ) : (
+        <p>no feedback given</p>
+      )}
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function onGood() {
+    const updatedCount = good + 1;
+    setGood(updatedCount);
+  }
+
+  function onNeutral() {
+    const updatedCount = neutral + 1;
+    setNeutral(updatedCount);
+  }
+
+  function onBad() {
+    const updatedCount = bad + 1;
+    setBad(updatedCount);
+  }
 
   return (
     <>
@@ -55,13 +74,7 @@ const App = () => {
         <button onClick={() => onNeutral()}>neutral</button>
         <button onClick={() => onBad()}>bad</button>
       </div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {averageScore() || 0}</p>
-      <p>positive {positivePercentage() || 0}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
