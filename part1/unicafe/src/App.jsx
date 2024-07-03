41698667cf5de46eca93103e64e232cd5f2e1a1b
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-03 15:55:35 +0600

    basic setup of unicafe of part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function onGood() {
+    const updatedCount = good + 1;
+    setGood(updatedCount);
+  }
+
+  function onNeutral() {
+    const updatedCount = neutral + 1;
+    setNeutral(updatedCount);
+  }
+
+  function onBad() {
+    const updatedCount = bad + 1;
+    setBad(updatedCount);
+  }
+
+  const all = good + neutral + bad;
+
+  function averageScore() {
+    const points = {
+      good: 1,
+      neutral: 0,
+      bad: -1,
+    };
+
+    const goodPoints = good * points.good;
+    const neutralPoints = neutral * points.neutral;
+    const badPoints = bad * points.bad;
+
+    const totalPoints = goodPoints + neutralPoints + badPoints;
+
+    const averagePoints = totalPoints / all;
+
+    return averagePoints;
+  }
+
+  function positivePercentage() {
+    const percentage = (good / all) * 100;
+
+    return percentage;
+  }
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => onGood()}>good</button>
+        <button onClick={() => onNeutral()}>neutral</button>
+        <button onClick={() => onBad()}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {averageScore() || 0}</p>
+      <p>positive {positivePercentage() || 0}%</p>
+    </>
+  );
+};
+
+export default App;
