ee08de4dedbd2355fa091cf5aed4f69947be7e42
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-04 06:03:49 +0600

    final update

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,43 +1,37 @@
 import { useState } from "react";
 
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, statistic }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{statistic}</td>
+    </tr>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = `${(good / all) * 100}%`;
 
-  function averageScore() {
-    const points = {
-      good: 1,
-      neutral: 0,
-      bad: -1,
-    };
-
-    const goodPoints = good * points.good;
-    const neutralPoints = neutral * points.neutral;
-    const badPoints = bad * points.bad;
-
-    const totalPoints = goodPoints + neutralPoints + badPoints;
-
-    const averagePoints = totalPoints / all;
-
-    return averagePoints;
-  }
-
-  function positivePercentage() {
-    const percentage = (good / all) * 100;
-
-    return percentage;
-  }
   return (
     <>
       <h1>statistics</h1>
       {all > 0 ? (
-        <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {all}</p>
-          <p>average {averageScore() || 0}</p>
-          <p>positive {positivePercentage() || 0}%</p>
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="good" statistic={good} />
+            <StatisticLine text="neutral" statistic={neutral} />
+            <StatisticLine text="bad" statistic={bad} />
+            <StatisticLine text="all" statistic={all} />
+            <StatisticLine text="average" statistic={average} />
+            <StatisticLine text="positive" statistic={positive} />
+          </tbody>
+        </table>
       ) : (
         <p>no feedback given</p>
       )}
@@ -46,7 +40,6 @@ const Statistics = ({ good, neutral, bad }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -70,9 +63,9 @@ const App = () => {
     <>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => onGood()}>good</button>
-        <button onClick={() => onNeutral()}>neutral</button>
-        <button onClick={() => onBad()}>bad</button>
+        <Button text="good" onClick={onGood} />
+        <Button text="neutral" onClick={onNeutral} />
+        <Button text="bad" onClick={onBad} />
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
