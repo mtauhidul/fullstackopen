de696e8d9ca173cd20c01663d567b3895d82465e
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-04 17:42:15 +0600

    added functionalities to phonebook and refactored code

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,120 @@
 import { useState } from "react";
 
-const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
-
+const Filter = ({ filter, filterChangeHandler }) => {
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+    <>
+      filter shown with <input value={filter} onChange={filterChangeHandler} />
+    </>
+  );
+};
+
+const Form = ({ submitHandler, newName, newNumber, onChangeHandler }) => {
+  return (
+    <>
+      <h2>add a new</h2>
+      <form onSubmit={submitHandler}>
         <div>
-          name: <input />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => onChangeHandler(event, "name")}
+          />
+          <br />
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(event) => onChangeHandler(event, "number")}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+    </>
+  );
+};
+
+const Persons = ({ shownPersons }) => {
+  return (
+    <>
       <h2>Numbers</h2>
-      ...
+      {shownPersons.map((person) => (
+        <Person key={person.id} person={person} />
+      ))}
+    </>
+  );
+};
+
+const Person = ({ person }) => {
+  return (
+    <p>
+      {person.name} {person.number}
+    </p>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const onChangeHandler = (event, type) => {
+    if (type == "name") {
+      setNewName(event.target.value);
+    }
+
+    if (type == "number") {
+      setNewNumber(event.target.value);
+    }
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const isDuplicate = persons.find((person) => person.name == newName);
+
+    if (!isDuplicate) {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    } else {
+      alert(`Name "${newName}" already exists!`);
+    }
+  };
+
+  const filterChangeHandler = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const shownPersons = !filter
+    ? persons
+    : persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLocaleLowerCase())
+      );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} filterChangeHandler={filterChangeHandler} />
+      <Form
+        submitHandler={submitHandler}
+        newName={newName}
+        newNumber={newNumber}
+        onChangeHandler={onChangeHandler}
+      />
+      <Persons shownPersons={shownPersons} />
     </div>
   );
 };
