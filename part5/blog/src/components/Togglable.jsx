a699a5342ef643ccbb2c58f59276d9863361dd4f
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2024-07-14 22:17:53 +0600

    added eslint and prop-types

--- part5/blog/src/components/Togglable.jsx
+++ part5/blog/src/components/Togglable.jsx
@@ -1,8 +1,16 @@
+import PropTypes from "prop-types";
 import React, { forwardRef, useImperativeHandle, useState } from "react";
 
 const Togglable = forwardRef((props, refs) => {
   const [isVisible, setIsVisible] = useState(false);
   const { buttonLabelShow, buttonLabelHide, children } = props;
+
+  Togglable.displayName = "Togglable";
+
+  Togglable.propTypes = {
+    buttonLabelShow: PropTypes.string.isRequired,
+    buttonLabelHide: PropTypes.string.isRequired,
+  };
 
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
